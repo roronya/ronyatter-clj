f13460ee615c240b9e1cc80205c6a2cfec3fed72
roronya <roronya@localhost.localdomain>
2014-02-04 21:55:04 +0900

    listenerをdefで定義。

--- src/ronyatter_clj/core.clj
+++ src/ronyatter_clj/core.clj
@@ -28,33 +28,35 @@
   (let [twitter (make-twitter)]
     (-> twitter (.getHomeTimeline)))))
 
+(def listener
+  (reify 
+    UserStreamListener
+    (onStatus [this status]
+      (println (-> (-> status (.getUser)) (.getScreenName)))
+      (println (-> status (.getText)))
+      (println))
+    (onDeletionNotice [this statusDeletionNotice] nil)
+    (onTrackLimitationNotice [this numberOfLimitedStatuses] nil)
+    (onScrubGeo [this userId upToStatusId] nil)
+    (onStallWarning [this warning] this)
+    (onFriendList [this friendIds] nil)
+    (onFavorite [this source target favoritedStatus] nil)
+    (onUnfavorite [this source rarget favoritedStatus] nil)
+    (onFollow [this source followedUser] nil)
+    (onDirectMessage [this directMessge])
+    (onUserListMemberAddition [this addedMember listOwner alist] nil)
+    (onUserListSubscription [this subscriver listOwner alist] nil)
+    (onUserListUnsubscription [this subscriber listOwner alist] nil)
+    (onUserListCreation [this listOwner alist] nil)
+    (onUserListUpdate [this listOwner alist] nil)
+    (onUserListDeletion [this listOwner alist] nil)
+    (onUserProfileUpdate [this updateUser] nil)
+    (onBlock [this source blockedUser] nil)
+    (onUnblock [this source unblockedUser] nil)
+    (onException [this ex] nil)))
+
 (defn -main []
-  (let [listener (reify 
-                   UserStreamListener
-                   (onStatus [this status]
-                     (println (-> (-> status (.getUser)) (.getScreenName)))
-                     (println (-> status (.getText)))
-                     (println))
-                   (onDeletionNotice [this statusDeletionNotice] nil)
-                   (onTrackLimitationNotice [this numberOfLimitedStatuses] nil)
-                   (onScrubGeo [this userId upToStatusId] nil)
-                   (onStallWarning [this warning] this)
-                   (onFriendList [this friendIds] nil)
-                   (onFavorite [this source target favoritedStatus] nil)
-                   (onUnfavorite [this source rarget favoritedStatus] nil)
-                   (onFollow [this source followedUser] nil)
-                   (onDirectMessage [this directMessge])
-                   (onUserListMemberAddition [this addedMember listOwner alist] nil)
-                   (onUserListSubscription [this subscriver listOwner alist] nil)
-                   (onUserListUnsubscription [this subscriber listOwner alist] nil)
-                   (onUserListCreation [this listOwner alist] nil)
-                   (onUserListUpdate [this listOwner alist] nil)
-                   (onUserListDeletion [this listOwner alist] nil)
-                   (onUserProfileUpdate [this updateUser] nil)
-                   (onBlock [this source blockedUser] nil)
-                   (onUnblock [this source unblockedUser] nil)
-                   (onException [this ex] nil))
-        twitterStream (make-twitterStream)]
+  (let [twitterStream (make-twitterStream)]
     (-> twitterStream (.addListener listener))
     (-> twitterStream (.user))))
 
