f83dd12cd0f54fd8c88b7f595440d31065f2e682
roronya <roronya@localhost.localdomain>
2014-02-04 12:49:20 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject ronyatter-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 [org.twitter4j/twitter4j-core "[3.0,)"]
+                 [org.twitter4j/twitter4j-stream "[3.0,)"]
+                 [org.twitter4j/twitter4j-async "[3.0,)"]]
+  :main ronyatter-clj.core
+)
---
+++ test/ronyatter_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns ronyatter-clj.core-test
+  (:require [clojure.test :refer :all]
+            [ronyatter-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
